949797ab2ab6dd658d332a11cb936d165e3ca1ef
Sarah Moss <sarah-moss@live.com>
2022-04-30 19:50:11 +0000

    Begin fetchMyIp function implementation

---
+++ iss.js
@@ -0,0 +1,15 @@
+// will contain most of the logic for fetching the data from each API endpoint
+
+
+
+
+const fetchMyIP = function(callback) { 
+  // use request to fetch IP address from JSON API
+  // asynchronously return our IP Address using an API
+  // pass through the error to the callback if an error occurs when requesting the IP data
+  // parse and extract the IP address using JSON and then pass that through to the callback (as the second argument) if there is no error
+
+}
+
+module.exports = { fetchMyIP };
+
