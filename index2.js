4a41f7df81e52749e76fc93aed0d90b4675dae53
Sarah Moss <sarah-moss@live.com>
2022-05-02 00:42:31 +0000

    Add fetchCoordsByIP

--- index2.js
+++ index2.js
@@ -1,4 +1,6 @@
-const { fetchMyIP } = require('./iss_promised');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss_promised');
 
 fetchMyIP()
+  .then(fetchCoordsByIP)
   .then(body => console.log(body));
+  
--- iss_promised.js
+++ iss_promised.js
@@ -8,5 +8,13 @@ const fetchMyIP = function() {
   return request('https://api.ipify.org?format=json');
 };
 
+// * Makes a request to freegeoip.app using the provided IP address, to get its geographical information (latitude/longitude)
+// * Input: JSON string containing the IP address
+// * Returns: Promise of request for lat/lon
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://freegeoip.app/json/${ip}`);
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
